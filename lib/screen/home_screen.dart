ccd97071e6ffa424681529de0ddee8ef4d1ca8f2
aloRanking <adigunlateef02@gmail.com>
2021-08-13 17:21:16 +0100

    precached image for better scrolling performance

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,38 +1,89 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio_app/pages/contactPage.dart';
-import 'package:portfolio_app/pages/homePage.dart';
+import 'package:portfolio_app/pages/aboutPage.dart';
 import 'package:portfolio_app/pages/portfolio.dart';
 import 'package:portfolio_app/pages/servicePage.dart';
 import 'package:portfolio_app/pages/testimonialPage.dart';
 import 'package:portfolio_app/pages/topNavbar.dart';
+import 'package:portfolio_app/utils/strings.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  //project images
+  late Image work1Image;
+  late Image work2Image;
+  late Image work3Image;
+  late Image work4Image;
+  late Image work5Image;
+  late Image tst1Image;
+  late Image tst2Image;
+  late Image profleImage;
+
+  @override
+  void initState() {
+    initImages();
+
+
+    super.initState();
+  }
+
+  void initImages() {
+     profleImage = Image.asset(kProfileImageUrl);
+    work1Image =Image.asset(kProjectImageUrl1);
+    work2Image = Image.asset(kProjectImageUrl2);
+    work3Image = Image.asset(kProjectImageUrl3);
+    work4Image = Image.asset(kProjectImageUrl4);
+    work5Image = Image.asset(kProjectImageUrl5);
+    tst1Image = Image.asset(ktestimonialImageUrl1);
+    tst2Image = Image.asset(ktestimonialImageUrl2);
+  }
+
+  @override
+  void didChangeDependencies() {
+  
+    super.didChangeDependencies();
+    preLoadImages();
+
+  }
+
+  void preLoadImages() {
+     precacheImage(profleImage.image, context);
+    precacheImage(work1Image.image, context);
+    precacheImage(work2Image.image, context);
+    precacheImage(work3Image.image, context);
+    precacheImage(work4Image.image, context);
+    precacheImage(work5Image.image, context);
+    precacheImage(tst1Image.image, context);
+    precacheImage(tst1Image.image, context);
+    precacheImage(tst2Image.image, context);
+  }
+
+
   @override
   Widget build(BuildContext context) {
     ScrollController scrollController = ScrollController();
     return Scaffold(
       backgroundColor: Colors.grey[100],
-      body: Column(
-        children: [
-          Expanded(
-            child: Scrollbar(
-              child: ListView(
-                controller: scrollController,
-                physics: BouncingScrollPhysics(),
-                children: [
-                  TopNavBar(controller: scrollController),
-                  HomePage(),
-                  ServicesPage(),
-                  PortfolioPage(),
-                  TestimonialPage(),
-                  ContactPage(
-                    scrollController: scrollController,
-                  )
-                ],
-              ),
-            ),
-          ),
-        ],
+      body: Scrollbar(
+        child: ListView(
+          controller: scrollController,
+          physics: BouncingScrollPhysics(),
+          children: [
+            TopNavBar(controller: scrollController),
+            AboutPage(),
+            ServicesPage(),
+            PortfolioPage(),
+            TestimonialPage(),
+            ContactPage(
+              scrollController: scrollController,
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/utils/strings.dart
+++ lib/utils/strings.dart
@@ -1,12 +1,16 @@
 const whatIDo =
     'Mauris neque libero, aliquet vel mollis nec, euismod sed tellus. Mauris convallis dictum elit id volutpat. Vivamus blandit, dolor vitae lacinia maximus, risus velit vehicula odio, a tincidunt turpis turpis tempus ex';
 const aboutMe_txt =
-    'I am a Mobile app developer with 3+ years hands-on experience using Dart and Flutter to develop mobile applications. I am highly skilled in designing, developing and testing softwares, I have thorough understanding of data structures and algorithms, have knowledge of back-end development best practices, State management, integrating Restful APIs, Firebase, Git/Github and hands-on software troubleshooting experience.';
+    '''I am an application software developer with over 4 years hands-on experience using Android sdk,Dart + Flutter to develop mobile applications. I am highly skilled in designing, developing and testing softwares. 
+I have the understanding of data structures and algorithms, the knowledge of back-end development best practices, state management, integrating restful APIs, Firebase, version control systems etc and  also hands-on software troubleshooting experience.''';
 
 const app_dev =
-    'Mobile application development is the process of creating apps that run on any mobile platform: Android and iOS. Mobile app development is the act or process by which a mobile app is developed for mobile devices, such as personal digital assistants, enterprise digital assistants or mobile phones.';
+    'I specialize in developing efficient apps that are beautiful and work across all platform (Andriod, iOS and Web)';
 const web_dev =
-    'Web applications are software developed for a specific purpose, hosted on a web server. They can be accessed using any internet-enabled device, like phones, laptops, and desktops.';
+    'Web applications are software developed for a specific purpose, hosted on a web server. I can also develop website, web apps using Flutter framework or the HTML,CSS and Javascript.';
+
+const train_dev = 'I also tutor people on web development and mobile app development.';
+const anime_dev = '';
 
 //links
 const String github = 'https://github.com/aloranking/';
@@ -17,4 +21,15 @@ const String lfds = 'https://github.com/aloRanking/lassa_fever_diagnostic_system
 const String foodapp = 'https://github.com/aloRanking/flutter_ui_challenges';
 const String movieapp = 'https://github.com/aloRanking/movie_app';
 const String prjctN = 'https://play.google.com/store/apps/details?id=com.aloranking.projectnigeria';
+
+//add optimized images for better load time and smooth scroll
+const String kProfileImageUrl = 'assets/images/aloranking.jpeg';
+final String kProjectImageUrl1 = 'assets/images/pjctsnap.jpg';
+final String kProjectImageUrl2 = 'assets/images/hagglexsnap.jpg';
+final String kProjectImageUrl3 = 'assets/images/moviesnap.jpg';
+final String kProjectImageUrl4 = 'assets/images/foodsnap.jpg';
+final String kProjectImageUrl5 = 'assets/images/lfdssnap.jpg';
+
+const String ktestimonialImageUrl1 = 'assets/images/user_2.png';
+const String ktestimonialImageUrl2 = 'assets/images/user_3.png';
 
